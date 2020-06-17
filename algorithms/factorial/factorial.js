361db35aa0f1e88c68ed7a166f338e491b3cfb59
agohorel <3317203+agohorel@users.noreply.github.com>
2020-06-17 17:54:02 -0400

    add memoized factorial examples

--- algorithms/factorial/factorial.js
+++ algorithms/factorial/factorial.js
@@ -1,3 +1,9 @@
+/* 
+The first two solutions have very poor runtimes due to repeating the same calculations 
+over and over. The third example utilizes 'memoization' by using a cache that 
+stores previously calculated values so each calculation only has to be performed once.
+*/
+
 function iterativeFactorial(n) {
   let factorial = 1;
   for (let i = n; i > 0; i--) {
@@ -13,7 +19,24 @@ function recursiveFactorial(n) {
   return n * recursiveFactorial(n - 1);
 }
 
+function memoizedFactorial(n, cache) {
+  let factorial = 1;
+
+  for (let i = n; i > 0; i--) {
+    if (!cache[i]) {
+      factorial *= i;
+      cache[i] = factorial;
+    } else {
+      factorial = cache[i];
+    }
+  }
+  return factorial;
+}
+
 const f1 = iterativeFactorial(5);
 const f2 = recursiveFactorial(5);
 
-console.log(f1, f2);
+const cache = {};
+const f3 = memoizedFactorial(100, cache);
+
+console.log(f1, f2, f3);
--- algorithms/factorial/factorial.py
+++ algorithms/factorial/factorial.py
@@ -1,8 +1,16 @@
+"""
+The first two solutions have very poor runtimes due to repeating the same calculations 
+over and over. The third example utilizes 'memoization' by using a cache that 
+stores previously calculated values so each calculation only has to be performed once.
+"""
+
+
 def iterative_factorial(n):
     factorial = 1
     for x in range(n, 0, -1):
-        factorial *= i
+        factorial *= x
     return factorial
+
 
 def recursive_factorial(n):
     if n == 0:
@@ -10,7 +18,24 @@ def recursive_factorial(n):
     return n * recursive_factorial(n-1)
 
 
+def memoized_factorial(n, cache):
+    factorial = 1
+
+    for x in range(n, 0, -1):
+        if x not in cache:
+            factorial *= x
+            cache[x] = factorial
+        else:
+            factorial = cache[x]
+
+    return factorial
+
+
+
 f1 = iterative_factorial(3)
 f2 = recursive_factorial(3)
 
-print(f1, f2)
+cache = {}
+f3 = memoized_factorial(100, cache)
+
+print(f1, f2, f3)
